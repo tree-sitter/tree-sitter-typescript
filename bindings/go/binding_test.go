82d8e4f8c1f4f8bf32254425446deb490de5375e
Segev Finer <segev@swimm.io>
2024-04-03 11:11:54 +0300

    Fixes

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -7,9 +7,16 @@ import (
 	"github.com/tree-sitter/tree-sitter-typescript"
 )
 
-func TestCanLoadGrammar(t *testing.T) {
-	language := tree_sitter.NewLanguage(tree_sitter_typescript.Language())
+func TestCanLoadTypeScriptGrammar(t *testing.T) {
+	language := tree_sitter.NewLanguage(tree_sitter_typescript.TypeScriptLanguage())
 	if language == nil {
 		t.Errorf("Error loading Typescript grammar")
+	}
+}
+
+func TestCanLoadTSXGrammar(t *testing.T) {
+	language := tree_sitter.NewLanguage(tree_sitter_tsx.TSXLanguage())
+	if language == nil {
+		t.Errorf("Error loading TSX grammar")
 	}
 }
